078b0bc48aaf811448b5e2cc6e1e1d2393ad73c4
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2021-04-02 08:53:53 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,7 +65,8 @@ $(window).on('load', function () {
     ]
   });
 
-  /*$('.homepage-slider').slick()({
+  /*TODO
+  $('.homepage-slider').slick()({
     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
   })*/
